c3e3b28ca2304057596920d26e140fe2a8ede892
kele <chj.ymh@gmail.com>
2021-04-09 13:34:50 +0800

    bye

--- public/main/index.js
+++ public/main/index.js
@@ -1,9 +1,10 @@
-const ipc = require('./ipc')
+const SerialPort = require('serialport'); 
 
-
+const ipc = require('./ipc')  
 module.exports.init=function() {
 	console.log("main init");
 	//加载所有ipc服务
 	ipc.setup()
+    console.log(SerialPort);
 }
 
